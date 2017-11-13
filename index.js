a52b5e01612231f5c32cf2e54c6390226f4745d9
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-11-13 13:24:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,63 @@
 var https = require('https');
 var url = require('url');
 
-const datadogApiKey = process.env.datadogApiKey;
-///////////////////////////////////////////////////////////////////////////////
-const datadogEndpoint = "https://app.datadoghq.com/api/v1/events"; // for testing "https://requestb.in";
-
-///////////////////////////////////////////////////////////////////////////////
-
 exports.handler = (event, context, callback) => {
-  
-    console.log(JSON.stringify(event,2,2));
-    
-    requestHttps({
-      method: 'POST', 
+
+  console.log('Event:\n' + JSON.stringify(event, 2, 2));
+
+  const datadogApiKey = event.headers['X-Gitlab-Token'];
+  const gitlabEventType = event.headers['X-Gitlab-Event'];
+  const gitlabEvent = JSON.parse(event.body);
+
+  let datadogEndpoint;
+  let datadogData;
+  switch (gitlabEventType) {
+    case 'Pipeline Hook':
+      datadogEndpoint = 'https://app.datadoghq.com/api/v1/events';
+      const pipelineStatusToDatadogAlertTypeMap = {
+        running: "info",
+        pending: "info",
+        success: "success",
+        failed: "error",
+        canceled: "warning",
+        skipped: "info",
+      }
+      datadogData = {
+        host: 'gitlab.com',
+        source_type_name: 'gitlab',
+        title: 'gitlab' +
+          ' / ' + gitlabEvent.project.namespace +
+          ' / ' + gitlabEvent.project.name +
+          ' / ' + gitlabEvent.object_attributes.ref +
+          ' / pipeline ' + gitlabEvent.object_attributes.status.toUpperCase(),
+        text: gitlabEvent.commit.url,
+        priority: 'normal',
+        alert_type: pipelineStatusToDatadogAlertTypeMap[gitlabEvent.object_attributes.status],
+        aggregation_key: 'gitlab/' + gitlabEvent.project.path_with_namespac + '/' + gitlabEvent.object_attributes.ref + '/pipeline',
+        tags: [
+          'project:' + gitlabEvent.project.path_with_namespace,
+          'pipeline:' + gitlabEvent.object_attributes.ref
+        ]
+      }
+      break;
+    default:
+      console.log('unsupported event: ' + gitlabEventType)
+      callback(null, {
+        statusCode: 400,
+        body: 'unsupported event: ' + gitlabEventType
+      });
+      return;
+  }
+
+  requestHttps({
+      method: 'POST',
       url: datadogEndpoint + "?api_key=" + datadogApiKey,
-      body: JSON.stringify({
-        "title": "Did you hear the news today?",
-        "text": "Oh boy!",
-        "priority": "normal",
-        "tags": [ "" ],
-        "alert_type": "info",
-        "aggregation_key": "GitlabPipelineID",
-        "source_type_name": "gitlab"
-      })
-    }, 
+      body: JSON.stringify(datadogData)
+    },
     (error, result) => {
       callback(null, {
         statusCode: 200,
-        headers: { },
-        body: "Success"
+        body: result
       });
     });
 };
@@ -38,22 +67,22 @@ exports.handler = (event, context, callback) => {
 let requestHttps = function(requestDescription, callback) {
   const requestUrl = url.parse(requestDescription.url);
   const requestOptions = {
-      host: requestUrl.host,
-      port: requestUrl.port,
-      path: requestUrl.path,
-      method: requestDescription.method || 'GET',
-      headers: requestDescription.headers || {}
+    host: requestUrl.host,
+    port: requestUrl.port,
+    path: requestUrl.path,
+    method: requestDescription.method || 'GET',
+    headers: requestDescription.headers || {}
   };
   const request = https.request(requestOptions, (response) => {
-      let body = '';
-      console.log('Status:', response.statusCode);
-      console.log('Headers:', JSON.stringify(response.headers));
-      response.setEncoding('utf8');
-      response.on('data', (chunk) => body += chunk);
-      response.on('end', () => {
-          console.log('Body:', body);
-          callback(null, body);
-      });
+    // console.log('Response:', JSON.stringify(response));
+    console.log('Status:', response.statusCode);
+    let body = '';
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => body += chunk);
+    response.on('end', () => {
+      console.log('Body:', body);
+      callback(null, body);
+    });
   });
   request.on('error', callback);
   request.write(requestDescription.body);
